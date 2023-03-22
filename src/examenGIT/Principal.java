0dba2388d976530df5bad1de2bcac65b964e06dd
ZhiLong <zhilong.s314800@cesurformacion.com>
2023-03-22 17:37:54 +0100

    commit ramaC

--- src/examenGIT/Principal.java
+++ src/examenGIT/Principal.java
@@ -5,10 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		
 	System.out.println("Hola a tod@s");
-<<<<<<< HEAD
-	System.out.println("Examen de ED Trimestre 2.1");
-=======
-	System.out.println("Hola a tod@s");
->>>>>>> RamaB
+	
+
 	
 }}
